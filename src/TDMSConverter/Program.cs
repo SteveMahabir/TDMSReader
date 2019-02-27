fef495406a40ba0dabce1064a1b8cf7571c881ac
Stephen <sw.mahabir@gmail.com>
2019-02-26 23:06:44 -0800

    Update: added output for DateTime ticks for all properties

--- src/TDMSConverter/Program.cs
+++ src/TDMSConverter/Program.cs
@@ -1,4 +1,28 @@
-﻿using System;
+﻿/*
+ * Program name:        TDMS Reader
+ * 
+ * Purpose:             To read a TDMS file and manipulate the DateTime variable
+ *                      to output the most accurate value of the raw data.
+ * 
+ * Programmer:          NI Instruments
+ * 
+ * Program Additions:   Stephen M.
+ * 
+ * Last Additions:      Github v.2301:  Added extra DateTime manipulations
+ *                                      due to needing the most accurate 
+ *                                      DateTime values for all tdms properties.
+ * 
+ * 
+ * Date last edited:    26 Feb 2019
+ * 
+ * 
+ * Notes:               Program edited for the purposes for UVic Eng Department - M. Aigner test data
+ * 
+ * This program is property of NI Instruments (c) 2019
+ */
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,20 +32,19 @@ namespace TDMSConverter
 {
     class Program
     {
-        static void Main(string[] args)
+
+
+        /* ReadFile Subroutine
+         * 
+         * Purpose: Reads the Trial 31 file and outputs the data requested
+         * 
+         * Input: (int) number of lines of data requested
+         * 
+         * Output: (void) dumps raw data into text file on the C:\
+         */
+        public void ReadFile(int dataRequested)
         {
-            using (var output = new System.IO.StreamWriter(System.IO.File.Create(@"C:\export.txt")))
-            using (var tdms = new NationalInstruments.Tdms.File("trial_31.tdms"))
-            {
-                tdms.Open();
-
-                // Used this for practise
-
-                //foreach (var value in tdms.Groups["Noise data"].Channels["Noise_1"].GetData<double>())
-                  //  output.WriteLine(value);
-            }
-
-using (var output = new System.IO.StreamWriter(System.IO.File.Create(@"C:\tdms.overvw.txt")))
+            using (var output = new System.IO.StreamWriter(System.IO.File.Create(@"C:\tdms.overvw.txt")))
             using (var tdms = new NationalInstruments.Tdms.File("trial_31.tdms"))
             {
                 tdms.Open();
@@ -49,19 +72,22 @@ using (var output = new System.IO.StreamWriter(System.IO.File.Create(@"C:\tdms.o
                             {
                                 DateTime n = Convert.ToDateTime(property.Value);
                                 output.WriteLine("        {0}: {1}", "wf_start_time ticks value: ", n.Ticks);
+                                output.WriteLine("        {0}: {1}", "(attempt) wf_start_time binary value: ", n.Ticks);
                             }
                             if (temp.ToString() == "NI_ExpStartTimeStamp")
                             {
                                 DateTime n = Convert.ToDateTime(property.Value);
-                                output.WriteLine("        {0}: {1}", "wf_start_time ticks value: ", n.Ticks);
+                                output.WriteLine("        {0}: {1}", "NI_ExpStartTimeStamp ticks value: ", n.Ticks);
+                                output.WriteLine("        {0}: {1}", "(attempt) NI_ExpStartTimeStamp binary value: ", n.ToBinary());
                             }
                             if (temp.ToString() == "NI_ExpTimeStamp")
                             {
                                 DateTime n = Convert.ToDateTime(property.Value);
-                                output.WriteLine("        {0}: {1}", "wf_start_time ticks value: ", n.Ticks);
+                                output.WriteLine("        {0}: {1}", "NI_ExpTimeStamp ticks value: ", n.Ticks);
+                                output.WriteLine("        {0}: {1}", "(attempt) NI_ExpTimeStamp binary value: ", n.ToBinary());
                             }
                         }
-                            
+
                         output.WriteLine();
                     }
                 }
@@ -80,9 +106,9 @@ using (var output = new System.IO.StreamWriter(System.IO.File.Create(@"C:\tdms.o
 
                             if (DateTime.TryParse(value.ToString(), out result))
                             {
-                                 
+
                                 //DateTime* raw = &result;
-                                
+
                                 output.Write("          {0}", value);
                                 output.WriteLine("          {0} (Binary Value)", Convert.ToString(result.ToBinary(), 2));
                                 output.WriteLine("          {0} (Tick Value)", result.Ticks);
@@ -101,5 +127,42 @@ using (var output = new System.IO.StreamWriter(System.IO.File.Create(@"C:\tdms.o
                 }
             }
         }
+
+        // Main Method
+        static void Main(string[] args)
+        {
+            using (var output = new System.IO.StreamWriter(System.IO.File.Create(@"C:\export.txt")))
+            using (var tdms = new NationalInstruments.Tdms.File("trial_31.tdms"))
+            {
+                // Used this for practise
+
+                //  Testing noise in the NI Tdms example
+                //  foreach (var value in tdms.Groups["Noise data"].Channels["Noise_1"].GetData<double>())
+                //  output.WriteLine(value);
+
+                tdms.Open();
+
+                try
+                {
+                    // Attempted to output 5 lines of data
+                    Program c = new Program();
+                    c.ReadFile(5);
+                }
+                // Standard exception handling
+                catch (Exception e)
+                {
+                    // output exception to the console and file
+                    output.WriteLine("Error has occured: ", e.ToString());
+                }
+
+            }
+
+
+        }
+
+
     }
+
 }
+
+
